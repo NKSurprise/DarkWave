80b77571e59d5d2969971e82f617f48786a31175
NKSurprise <krasimiriosifov303@gmail.com>
2025-09-08 14:40:17 +0300

    Add basic TCP server implementation in Go
    Introduces a simple TCP server in main.go that listens on port 3000, accepts incoming connections, and prints received messages. Also adds a settings.JSON file with file encoding preferences.

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
+type Server struct {
+	listenAddr string
+	ln         net.Listener
+	quitch     chan struct{}
+}
+
+func NewServer(listenAddr string) *Server {
+	return &Server{
+		listenAddr: listenAddr,
+		quitch:     make(chan struct{}),
+	}
+}
+
+func (s *Server) Start() error {
+	ln, err := net.Listen("tcp", s.listenAddr)
+	if err != nil {
+		return err
+	}
+	defer ln.Close()
+	s.ln = ln
+
+	// run accept loop in another goroutine
+	go s.acceptLoop()
+
+	// block until quit signal is received
+	<-s.quitch
+	return nil
+}
+
+func (s *Server) acceptLoop() {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			return
+		}
+		go s.readLoop(conn)
+	}
+}
+
+func (s *Server) readLoop(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read error:", err)
+			return
+		}
+		msg := buf[:n]
+		fmt.Println("received:", string(msg))
+	}
+}
+
+func main() {
+	server := NewServer(":3000")
+	log.Fatal(server.Start())
+}
